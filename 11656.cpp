991b9c568aad14361e308fb18bfc7293734cd5b6
Lee Junyoung <dlwnsdud12288@gmail.com>
2022-03-23 15:39:13 +0900

    2022.03.23_2

---
+++ 11656.cpp
@@ -0,0 +1,25 @@
+/*
+    2022.03.23
+    11656번 접미사 배열
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+int main(){
+    string S;
+    cin >> S;
+    vector<string> suf(S.size());
+    for(int i = 0; i < S.size(); i++){
+        for(int j = i; j < S.size(); j++){
+            suf[i] += S[j];
+        }
+    }
+    sort(suf.begin(),suf.end());
+    for(int i = 0; i < S.size(); i++){
+        cout << suf[i] << '\n';
+    }
+    return 0;
+}
--- 1251.cpp
+++ 1251.cpp
@@ -17,7 +17,7 @@ int main(){
     b = a;
     for(int i = 0; i < a.size()-1; i++){
         for(int j = i+1; j < a.size()-1; j++){
-            reverse(i,j);;
+            reverse(i,j);
             if(a < m){
                 turn = true;
                 m = a;
