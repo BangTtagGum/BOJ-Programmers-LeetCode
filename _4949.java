1a4657c3dff939195fb08b933f8a5e21e7957886
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-10-28 10:45:32 +0900

    2023.10.28

--- _4949.java
+++ _4949.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
  * 4949 괄호
  */
 public class Main {
-
+ 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
