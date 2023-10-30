b26bbcd50fc2ae7318b6626f02ad69d599087d4b
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-10-30 10:22:05 +0900

    2023.10.30

--- _1927.java
+++ _1927.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
+ 
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
