f0b9db51ba27f0f5124ddd4fe44591e1b0c2719a
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-11-16 15:08:52 +0900

    2023.11.16

--- _2869.java
+++ _2869.java
@@ -5,7 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.StringTokenizer; 
 
 /**
  * 2023.07.24
