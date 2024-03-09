3568486c43cd47b2128b9baea42f3930c04cb814
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-03-09 15:52:51 +0900

    Time: 15 ms (27.92%), Space: 44.6 MB (40.8%) - LeetHub

--- 리트코드/0125-valid-palindrome/0125-valid-palindrome.java
+++ 리트코드/0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,10 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        int start = 0;
-        int end = s.length() - 1;
-
-        while (start < end) {
-            if (!Character.isLetterOrDigit(s.charAt(start))) {
-                start++;
-            } else if (!Character.isLetterOrDigit(s.charAt(end))) {
-                end--;
-            } else {
-                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(
-                        s.charAt(end))) {
-                    return false;
-                }
-                start++;
-                end--;
-            }
-        }
-        return true;
+    /**
+     * 문자열 직접 비교
+     */
+    public static boolean isPalindrome(String s) {
+        String s_filtered = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String s_reversed = new StringBuilder(s_filtered).reverse().toString();
+        return s_filtered.equals(s_reversed);
     }
 }
