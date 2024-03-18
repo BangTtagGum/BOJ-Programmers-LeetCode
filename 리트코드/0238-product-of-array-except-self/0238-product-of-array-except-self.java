d6f9ee3c27f82e0b09e9492f075c8c3f13f41741
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-03-18 21:20:35 +0900

    Time: 188 ms (6.04%), Space: 53 MB (78.44%) - LeetHub

---
+++ 리트코드/0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
+
+        int p = 1;
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = p;
+            p *= nums[i];
+        }
+        p = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] *= p;
+            p *= nums[i];
+        }
+
+        for (int i : answer) {
+            System.out.println(i);
+        }
+
+        return answer;
+    }
+}
