da7feba8bd747682f6e706493fac1debe2cd110d
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-03-19 19:26:49 +0900

    Time: 316 ms (41.83%), Space: 47.9 MB (80.55%) - LeetHub

---
+++ 리트코드/0238-product-of-array-except-self/0238-product-of-array-except-self.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+    val answer = IntArray(nums.size)
+
+    var p = 1
+    for (i in nums.indices) {
+        answer[i] = p
+        p *= nums[i]
+    }
+
+    p = 1
+    for (i in nums.indices.reversed()) {
+        answer[i] *= p
+        p *= nums[i]
+    }
+    return answer
+}
+}
