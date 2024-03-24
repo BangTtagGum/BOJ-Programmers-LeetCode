54aa8de3b0c7eff94d7d7a3d361083938cb3e02b
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-03-24 10:41:26 +0900

    Time: 10 ms (5.01%), Space: 45.1 MB (5.78%) - LeetHub

--- 리트코드/0002-add-two-numbers/0002-add-two-numbers.java
+++ 리트코드/0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,26 +1,45 @@
+import java.math.BigInteger;
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0); // creating an dummy list
-        ListNode curr = dummy; // intialising an pointer
-        int carry = 0; // intialising our carry with 0 intiall
-        // while loop will run, until l1 OR l2 not reaches null OR if they both reaches null. But our carry has some value in it. 
-		// We will add that as well into our list
-        while(l1 != null || l2 != null || carry == 1){
-            int sum = 0; // intialising our sum
-            if(l1 != null){ // adding l1 to our sum & moving l1
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if(l2 != null){ // adding l2 to our sum & moving l2
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            sum += carry; // if we have carry then add it into our sum
-            carry = sum/10; // if we get carry, then divide it by 10 to get the carry
-            ListNode node = new ListNode(sum % 10); // the value we'll get by moduloing it, will become as new node so. add it to our list
-            curr.next = node; // curr will point to that new node if we get
-            curr = curr.next; // update the current every time
+        ListNode reverseL1 = reverseNode(l1);
+        ListNode reverseL2 = reverseNode(l2);
+
+        BigInteger answer = toBigInteger(reverseL1).add(toBigInteger(reverseL2));
+
+        return toReverseLinkedList(answer);
+    }
+
+    public ListNode toReverseLinkedList(BigInteger val){
+        ListNode prev = null, node = null;
+
+        for(char c : String.valueOf(val).toCharArray()){
+            node = new ListNode(Character.getNumericValue(c));
+
+            node.next = prev;
+            prev = node;
         }
-        return dummy.next; // return dummy.next bcz, we don't want the value we have consider in it intially!!
+        return node;
+    }
+
+    public BigInteger toBigInteger(ListNode node){
+        StringBuilder sb = new StringBuilder();
+        while(node != null){
+            sb.append(node.val);
+            node = node.next;
+        }
+        return new BigInteger(sb.toString());
+    }
+
+    public ListNode reverseNode(ListNode node){
+        ListNode prev = null;
+        while(node != null){
+            ListNode next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        
+        return prev;
     }
 }
