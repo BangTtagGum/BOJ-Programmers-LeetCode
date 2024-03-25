bc93165d64153b115905ecc50590eed7754e267b
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-03-25 16:13:34 +0900

    Time: 1 ms (100%), Space: 44.1 MB (89.31%) - LeetHub

--- 리트코드/0002-add-two-numbers/0002-add-two-numbers.java
+++ 리트코드/0002-add-two-numbers/0002-add-two-numbers.java
@@ -2,44 +2,30 @@ import java.math.BigInteger;
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode reverseL1 = reverseNode(l1);
-        ListNode reverseL2 = reverseNode(l2);
+        ListNode node = new ListNode(0);
 
-        BigInteger answer = toBigInteger(reverseL1).add(toBigInteger(reverseL2));
+        ListNode root = node;
 
-        return toReverseLinkedList(answer);
-    }
+        int sum, carry = 0, remainder;
 
-    public ListNode toReverseLinkedList(BigInteger val){
-        ListNode prev = null, node = null;
+        while(l1 != null || l2 != null || carry !=0){
+            sum = 0;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
 
-        for(char c : String.valueOf(val).toCharArray()){
-            node = new ListNode(Character.getNumericValue(c));
+            remainder = (sum + carry) % 10;
+            carry = (sum + carry) / 10;
 
-            node.next = prev;
-            prev = node;
-        }
-        return node;
-    }
-
-    public BigInteger toBigInteger(ListNode node){
-        StringBuilder sb = new StringBuilder();
-        while(node != null){
-            sb.append(node.val);
+            node.next = new ListNode(remainder);
             node = node.next;
         }
-        return new BigInteger(sb.toString());
-    }
-
-    public ListNode reverseNode(ListNode node){
-        ListNode prev = null;
-        while(node != null){
-            ListNode next = node.next;
-            node.next = prev;
-            prev = node;
-            node = next;
-        }
-        
-        return prev;
+    
+        return root.next;
     }
 }
