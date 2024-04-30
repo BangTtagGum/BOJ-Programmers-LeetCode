eb8bdbb1d753c35534c45c33409cfabfde5632b4
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-04-30 20:59:10 +0900

    Add: _0771-jewels-and-stones_HashMap ver

---
+++ 리트코드/0771-jewels-and-stones/0771-jewels-and-stones_HashMap.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Map<Character, Integer> freqs = new HashMap<>();
+
+        for (char c : stones.toCharArray()) {
+            if (freqs.containsKey(c)) {
+                freqs.put(c, freqs.get(c) + 1);
+            } else {
+                freqs.put(c, 1);
+            }
+        }
+
+        int answer = 0;
+        for (char c : jewels.toCharArray()) {
+            if(freqs.containsKey(c)){
+                answer += freqs.get(c);
+            }
+        }
+        return answer;
+    }
+}
