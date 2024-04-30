b3f78b687d6abbdaa818491ae756c4ca2d3f9fbf
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-04-30 20:52:23 +0900

    Time: 2 ms (12.74%), Space: 41.7 MB (52.06%) - LeetHub

---
+++ 리트코드/0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Map<Character, Integer> freqs = new HashMap<>();
+
+        for (char c : stones.toCharArray()) {
+            if (freqs.containsKey(c)) {
+                freqs.put(c, freqs.get(c) + 1);
+            } else {
+                freqs.put(c, 1);
+            }
+        }
+
+        int answer = 0;
+        for (char c : jewels.toCharArray()) {
+            answer += freqs.getOrDefault(c,0);
+        }
+        return answer;
+    }
+}
