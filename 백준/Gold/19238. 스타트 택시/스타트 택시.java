f4398cf5aa45a58a860c5b800a65af8db06061e4
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-05-01 16:39:07 +0900

    [Gold II] Title: 스타트 택시, Time: 228 ms, Memory: 26604 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.java
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.java
@@ -11,86 +11,48 @@ public class Main {
     static int n, totalClientCnt, fuel;
 
     static int[][] map;
-    static int[][] startPoint;
     static int[][] endPoint;
     static int[] dr = {-1, 0, 0, 1};
     static int[] dc = {0, -1, 1, 0};
+    static SmartTaxi smartTaxi;
 
     public static void main(String[] args) throws IOException {
 
-        // 택시에서 가장 가까운 승객 태우기 (동일한 거리일 경우 행이 작은 고객부터, 그래도 동일하면 열이 작은 순서)
 
-        // 승객을 이동시키면 연료 2배 충전 목적지에 도착과 동시에 연료가 바닥나면 성공한 것으로 간주
+        init();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        totalClientCnt = Integer.parseInt(st.nextToken());
-        fuel = Integer.parseInt(st.nextToken());
-
-        map = new int[n+1][n+1];
-        for (int i = 1; i <= n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        st = new StringTokenizer(br.readLine());
-
-        int startR = Integer.parseInt(st.nextToken());
-        int startC = Integer.parseInt(st.nextToken());
-
-        startPoint = new int[totalClientCnt + 2][2];
-        endPoint = new int[totalClientCnt + 2][2];
-        for (int i = 2; i < totalClientCnt + 2; i++) {
-            st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            map[r][c] = i;
-
-            startPoint[i][0] = r;
-            startPoint[i][1] = c;
-            endPoint[i][0] = Integer.parseInt(st.nextToken());
-            endPoint[i][1] = Integer.parseInt(st.nextToken());
-        }
-
-        SmartTaxi smartTaxi = new SmartTaxi(startR, startC, fuel, totalClientCnt);
         smartTaxi.work();
 
     }
-
-    static boolean isValid(int r, int c) {
-        return 1 <= r && r <= n && 1 <= c && c <= n && map[r][c] != 1;
-    }
-
     static class SmartTaxi {
 
         int r, c;
-        int fuel;
-        int totalClientCnt;
-        int completeClientCnt = 0;
 
+        int fuel;
+
+        int totalClientCnt;
+        int currentClientNum;
+        int completeClientCnt = 0;
         public SmartTaxi(int r, int c, int fuel, int totalClientCnt) {
             this.r = r;
             this.c = c;
             this.fuel = fuel;
             this.totalClientCnt = totalClientCnt;
         }
-
         void work() {
             while (totalClientCnt > completeClientCnt) {
-                int clientNum = pickup();
-
-                // 현재 연료로 픽업할 수 있는 고객이 없는 경우
-                if (clientNum == 0) {
+                // 가장 가까운 고객 픽업
+                if (!pickup()) {
+                    // 현재 연료로 픽업할 수 있는 고객이 없는 경우
                     System.out.println(-1);
                     return;
                 }
 
-                if (!moveClientToDestination(endPoint[clientNum][0], endPoint[clientNum][1])) {
+                map[r][c] = 0;
+
+                // 고객 이송
+                if (!moveClientToDestination(endPoint[this.currentClientNum][0], endPoint[this.currentClientNum][1])) {
+                    // 연료안에 이송이 불가능한 경우
                     System.out.println(-1);
                     return ;
                 }
@@ -98,13 +60,15 @@ public class Main {
             System.out.println(this.fuel);
         }
 
-        int pickup() {
-            // 가까운 승객 탐색
+        // 가까운 고객 탐색
+        boolean pickup() {
+            // 고객과 택시의 위치가 동일할 경우
             if (map[r][c] > 1) {
-                int clientNum = map[r][c];
-                map[r][c] = 0;
-                return clientNum;
+                this.currentClientNum = map[r][c];
+                return true;
             }
+
+            // 가까운 거리부터 탐색
             boolean[][] visited = new boolean[n+1][n+1];
             PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
                 if (a[0] == b[0]) {
@@ -139,7 +103,7 @@ public class Main {
                 }
             }
             if (pq.isEmpty()) {
-                return 0;
+                return false;
             }
             int[] poll = pq.poll();
             int r = poll[0];
@@ -150,8 +114,8 @@ public class Main {
             this.r = r;
             this.c = c;
             this.fuel -= usedFuel;
-            map[r][c] = 0;
-            return clientNum;
+            this.currentClientNum = clientNum;
+            return true;
         }
 
         boolean moveClientToDestination(int er, int ec) {
@@ -184,7 +148,48 @@ public class Main {
             }
             return false;
         }
+
+
+
+    }
+    static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        totalClientCnt = Integer.parseInt(st.nextToken());
+        fuel = Integer.parseInt(st.nextToken());
+
+        map = new int[n+1][n+1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        int startR = Integer.parseInt(st.nextToken());
+        int startC = Integer.parseInt(st.nextToken());
+
+        endPoint = new int[totalClientCnt + 2][2];
+        for (int i = 2; i < totalClientCnt + 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            map[r][c] = i;
+
+            endPoint[i][0] = Integer.parseInt(st.nextToken());
+            endPoint[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        smartTaxi = new SmartTaxi(startR, startC, fuel, totalClientCnt);
     }
 
+    static boolean isValid(int r, int c) {
+        return 1 <= r && r <= n && 1 <= c && c <= n && map[r][c] != 1;
+    }
 }
 
