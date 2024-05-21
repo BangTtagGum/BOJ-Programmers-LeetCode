88e03b8269d778a648b0338576bd6b7617c91d70
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-05-21 19:50:05 +0900

    Time: 43 ms (6.93%), Space: 47.6 MB (71.54%) - LeetHub

--- 리트코드/0743-network-delay-time/0743-network-delay-time.java
+++ 리트코드/0743-network-delay-time/0743-network-delay-time.java
@@ -1,36 +1,39 @@
 import java.util.AbstractMap.*;
 import java.util.*;
-
+import java.util.Map.Entry;
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+
         for (int[] time : times) {
             graph.putIfAbsent(time[0], new HashMap<>());
             graph.get(time[0]).put(time[1], time[2]);
         }
 
-        Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Map.Entry.comparingByValue());
-        pq.add(new SimpleEntry<>(k, 0));
+        Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((e1, e2) ->
+                e1.getValue() - e2.getValue()
+        );
 
-        Map<Integer, Integer> dist = new HashMap<>();
+        pq.add(new SimpleEntry<>(k, 0));
+        Map<Integer, Integer> distMap = new HashMap<>();
 
         while (!pq.isEmpty()) {
-            Map.Entry<Integer, Integer> cur = pq.poll();
-            int u = cur.getKey();
-            int dist_u = cur.getValue();
+            Entry<Integer, Integer> cur = pq.poll();
+            int node = cur.getKey();
+            int distance =cur.getValue();
 
-            if (!dist.containsKey(u)) {
-                dist.put(u, dist_u);
-                if (graph.containsKey(u)) {
-                    for (Map.Entry<Integer, Integer> v : graph.get(u).entrySet()) {
-                        int alt = dist_u + v.getValue();
-                        pq.add(new AbstractMap.SimpleEntry<>(v.getKey(), alt));
+            if (!distMap.containsKey(node)) {
+                distMap.put(node, distance);
+                if (graph.containsKey(node)) {
+                    for (Entry<Integer, Integer> e : graph.get(node).entrySet()) {
+                        pq.add(new SimpleEntry<>(e.getKey(), distance + e.getValue()));
                     }
                 }
             }
         }
-        if (dist.size() == n) {
-            return Collections.max(dist.values());
+
+        if (distMap.size() == n) {
+            return Collections.max(distMap.values());
         }
         return -1;
     }
