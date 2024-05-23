1ab5770a271bfbf824834f4d43c0c2731a853296
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-05-23 16:15:36 +0900

    Time: 18 ms (10.09%), Space: 45.3 MB (9.41%) - LeetHub

--- 리트코드/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 리트코드/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,49 +1,48 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.PriorityQueue;
-import java.util.Queue;
-
 class Solution {
     static public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
-        for (int[] flight : flights) {
-            graph.putIfAbsent(flight[0], new HashMap<>());
-            graph.get(flight[0]).put(flight[1], flight[2]);
+        Map<Integer, Map<Integer,Integer>> graph = new HashMap<>();
+
+        for (int[] flight : flights){
+            graph.putIfAbsent(flight[0],new HashMap<>());
+            graph.get(flight[0]).put(flight[1],flight[2]);
         }
 
-        Queue<int[]> pq = new PriorityQueue<>((e1, e2) ->
-                e1[1] - e2[1]
+        Queue<int[]> pq = new PriorityQueue<>((e1,e2) ->
+            e1[1] - e2[1]
         );
-
-        pq.add(new int[]{src, 0, 0});
 
         Map<Integer,Integer> visited = new HashMap<>();
 
-        while (!pq.isEmpty()) {
-            int[] poll = pq.poll();
-            int node = poll[0];
-            int distance = poll[1];
-            int stopoverCount = poll[2];
+        pq.add(new int[]{src,0,0});
+        
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int node = cur[0];
+            int dist = cur[1];
+            int visitCount = cur[2];
 
-            visited.put(node,stopoverCount);
-
-            if (node == dst) {
-                return distance;
+            if(node == dst){
+                return dist;
             }
+            visited.put(node,visitCount);
 
-            if (stopoverCount <= k && graph.containsKey(node)) {
-                stopoverCount++;
-            
-                for (Entry<Integer, Integer> e : graph.get(node).entrySet()) {
-                    if(!visited.containsKey(e.getKey()) || stopoverCount < visited.get(e.getKey())){
-                        pq.add(new int[]{e.getKey(), distance + e.getValue(), stopoverCount});
+            // 현재 노드에서 다음 경로가 있는 경우 pq 에 추가
+            if(visitCount <= k && graph.containsKey(node)){
+                for(Map.Entry<Integer,Integer> e : graph.get(node).entrySet()){
+                    // 처음 방문하는 노드거나 || 방문 했었지만, 아직 다른곳을 경유해 더 짧게 갈 가능성이 있는 경우
+                    if(!visited.containsKey(e.getKey()) || visitCount < visited.get(e.getKey())){
+                        pq.add(new int[]{e.getKey(),e.getValue() + dist, visitCount + 1});
                     }
                 }
             }
+            
         }
 
-
         return -1;
+
     }
 }
