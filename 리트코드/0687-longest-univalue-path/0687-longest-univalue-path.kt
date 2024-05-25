51a4d91884221ead8623ce9dcd0d608db18d30d3
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-05-25 15:33:44 +0900

    Time: 289 ms (66.67%), Space: 41.3 MB (9.52%) - LeetHub

---
+++ 리트코드/0687-longest-univalue-path/0687-longest-univalue-path.kt
@@ -0,0 +1,35 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun longestUnivaluePath(root: TreeNode?): Int {
+        var longest: Int = 0
+        fun dfs(node : TreeNode?):Int{
+            if(node == null){
+                return -1
+            }
+
+            var left = dfs(node.left)
+            var right = dfs(node.right)
+
+            if(left != -1 && node.left.`val` != node.`val` ){
+                left = -1
+            }
+            if(right != -1 && node.right.`val` != node.`val` ){
+                right = -1
+            }
+            longest = Math.max(longest, left + right + 2)
+            return Math.max(left,right) + 1
+        }
+        
+        dfs(root)
+        return longest
+    }
+}
