169933fe2c5eec49f9515d955a85c83449729313
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-05-29 15:45:08 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 4292 ms, Memory: 389876 KB -BaekjoonHub

---
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        // 도시간 도로의 거리 정보
+        Map<Integer, Map<Integer, ArrayList<Integer>>> roadMap = new HashMap<>();
+
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int dist = Integer.parseInt(st.nextToken());
+
+            roadMap.putIfAbsent(start, new HashMap<>());
+            roadMap.get(start).putIfAbsent(end, new ArrayList<>());
+            roadMap.get(start).get(end).add(dist);
+        }
+
+        Queue<int[]> pq = new PriorityQueue<>(
+                (e1, e2) -> e1[1] - e2[1]
+        );
+        pq.add(new int[]{1, 0});
+
+
+        // 도시까지의 최단거리 정보
+        Map<Integer, ArrayList<Integer>> distMap = new HashMap<>();
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int nodeNum = cur[0];
+            int dist = cur[1];
+
+            // 최단거리가 처음 지정되거나 ||  (k번째 최단거리까지 구해지지 않았고 && 저장된 최단거리 중 가장 먼 최단거리보다 멀게 도착하는 경우만)
+            if (!distMap.containsKey(nodeNum) || (distMap.get(nodeNum).size() < k
+                    && distMap.get(nodeNum).get(distMap.get(nodeNum).size() - 1) <= dist)) {
+                distMap.putIfAbsent(nodeNum, new ArrayList<>());
+                distMap.get(nodeNum).add(dist);
+                if (roadMap.containsKey(nodeNum)) {
+                    for (Entry<Integer, ArrayList<Integer>> entry : roadMap.get(nodeNum).entrySet()) {
+                        for (Integer value : entry.getValue()) {
+                            pq.add(new int[]{entry.getKey(), dist + value});
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (distMap.get(i) != null && distMap.get(i).size() == k) {
+                System.out.println(distMap.get(i).get(k - 1));
+            } else {
+                System.out.println(-1);
+            }
+        }
+
+    }
+}
