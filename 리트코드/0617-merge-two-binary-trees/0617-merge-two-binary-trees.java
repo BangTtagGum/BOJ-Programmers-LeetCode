44b90a9b8a3a5f11d93852f845ecd62d6c4f059c
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-05-31 16:48:13 +0900

    Time: 0 ms (100%), Space: 44.6 MB (83.39%) - LeetHub

--- 리트코드/0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 리트코드/0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -23,10 +23,10 @@ class Solution {
             return root1;
         }
         
-        TreeNode newNode = new TreeNode(root1.val + root2.val);
-        newNode.left = mergeTrees(root1.left, root2.left); 
-        newNode.right = mergeTrees(root1.right, root2.right); 
-
-        return newNode;
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left); ;
+        root1.right = mergeTrees(root1.right, root2.right); 
+        
+        return root1;
     }
 }
