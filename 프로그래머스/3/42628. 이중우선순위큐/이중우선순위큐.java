ac1d006c1c4175d4c295053fd82f22b9d6983213
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-06-10 18:49:40 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.75 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,73 @@
+import java.util.*;
+class Solution {
+    
+    static class MinMaxPriorityQueue{
+        Queue<Integer> minQueue = new PriorityQueue<>();
+        Queue<Integer> maxQueue = new PriorityQueue<>(Collections.reverseOrder());
+        
+        void operation(String instruct){
+            String[] op = instruct.split(" ");
+            if(op[0].equals("I")){
+                insert(Integer.parseInt(op[1]));
+                
+            } else if(op[0].equals("D")){
+                if(op[1].equals("1")){
+                    pollLargest();
+                } else if(op[1].equals("-1")){
+                    pollSmallest();
+                }
+            }
+        }
+        
+        void insert(int i){
+            minQueue.add(i);
+            maxQueue.add(i);
+        }
+        
+        int pollLargest(){
+            
+            if(maxQueue.isEmpty()){
+                return 0;
+            }
+            
+            Integer val = maxQueue.poll();
+            minQueue.remove(val);
+            
+            return val;
+        }
+        
+        int pollSmallest(){
+            
+            if(minQueue.isEmpty()){
+                return 0;
+            }
+            
+            Integer val = minQueue.poll();
+            maxQueue.remove(val);
+            
+            return val;
+        }
+        
+        
+    }
+    
+    
+    public int[] solution(String[] operations) {
+        MinMaxPriorityQueue mmpq = new MinMaxPriorityQueue();
+        
+        for(String operation : operations){
+            mmpq.operation(operation);
+        }
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        arr.add(mmpq.pollLargest());
+        arr.add(mmpq.pollSmallest());
+        
+        int[] answer = new int[arr.size()];
+        for(int i = 0; i < arr.size(); i++){
+            answer[i] = arr.get(i); 
+        }
+        return answer;
+    }
+}
