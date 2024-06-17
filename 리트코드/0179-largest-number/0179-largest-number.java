eaab532eb93863b0409525efc8914e85a5154855
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-06-17 18:16:50 +0900

    Time: 6 ms (62.83%), Space: 42.7 MB (85.48%) - LeetHub

--- 리트코드/0179-largest-number/0179-largest-number.java
+++ 리트코드/0179-largest-number/0179-largest-number.java
@@ -15,7 +15,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) s[i] = String.valueOf(nums[i]);
         Arrays.sort(s, (a,b) -> (b + a).compareTo(a + b));
         
-        if (nums[0] == 0) {
+        if (s[0].equals("0")) {
             return "0";
         } else {
             return String.join("",s);
