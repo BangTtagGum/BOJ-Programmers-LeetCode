0b5ad8b7c1eef0d96eeba7ef50c5e680041b7023
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-06-17 18:11:56 +0900

    Time: 44 ms (5.07%), Space: 45.4 MB (5.74%) - LeetHub

---
+++ 리트코드/0179-largest-number/0179-largest-number.java
@@ -0,0 +1,29 @@
+import java.math.BigInteger;
+
+class Solution {
+     public static boolean toSwap(int n1, int n2) {
+        if (new BigInteger(String.valueOf(n1) + n2).compareTo(new BigInteger(String.valueOf(n2) + n1)) < 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String largestNumber(int[] nums) {
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j > 0 ; j--) {
+                if (toSwap(nums[j - 1], nums[j])) {
+                    int tmp = nums[j];
+                    nums[j] = nums[j - 1];
+                    nums[j - 1] = tmp;
+                }
+            }
+        }
+
+        if (nums[0] == 0) {
+            return "0";
+        } else {
+            return Arrays.toString(nums).replaceAll("\\[|\\]|,|\\s", "");
+        }
+    }
+}
