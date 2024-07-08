bb8f39f7387561e2b487a6b13cd5ba157444eaea
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-07-08 14:54:23 +0900

    Time: 177 ms (58.02%), Space: 37.3 MB (75.43%) - LeetHub

---
+++ 리트코드/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+        var result = 0
+        for(i in 0 until prices.size - 1){
+            if(prices[i + 1] > prices[i]){
+                result += prices[i + 1] - prices[i]
+            }
+        }
+        return result
+    }
+}
