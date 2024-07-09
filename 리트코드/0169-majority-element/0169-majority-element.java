baad577db3fa471923d6198c27a941bd5310714b
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-07-09 14:37:21 +0900

    Time: 3 ms (62%), Space: 52.6 MB (66.99%) - LeetHub

--- 리트코드/0169-majority-element/0169-majority-element.java
+++ 리트코드/0169-majority-element/0169-majority-element.java
@@ -1,25 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        
-        return divideAndConquer(0,nums.length-1,nums);
-    }
-
-    public int divideAndConquer(int left, int right, int[] nums){
-        if(left == right){
-            return nums[left];
-        }
-        int mid = left + (right - left) / 2;
-        int a = divideAndConquer(left, mid, nums);
-        int b = divideAndConquer(mid + 1, right, nums);
-
-
-        int countA = 0;
-        for(int i = left; i <= right; i++){
-            if(nums[i] == a){
-                countA++;
-            }
-        }
-
-        return countA > (right - left) / 2 ? a : b;
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
     }
 }
