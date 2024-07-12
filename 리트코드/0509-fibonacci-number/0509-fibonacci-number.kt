251c077e87f4206ebf1aa85d637cefbed32a6195
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-07-12 14:38:03 +0900

    Time: 136 ms (31.27%), Space: 33.1 MB (39.79%) - LeetHub

---
+++ 리트코드/0509-fibonacci-number/0509-fibonacci-number.kt
@@ -0,0 +1,13 @@
+class Solution {
+    var dp:IntArray = IntArray(31)
+    fun fib(n: Int): Int {
+        if(n <= 1){
+            return n
+        }
+        if(dp[n] != 0){
+            return dp[n]
+        }
+        dp[n] = fib(n - 1) + fib(n - 2)
+        return dp[n]
+    }
+}
