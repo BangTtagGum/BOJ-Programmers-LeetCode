7d53ac3785bcd6eb92f9e56e1120e7d86b69f296
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-07-22 14:37:55 +0900

    [level 2] Title: 양궁대회, Time: 0.88 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -0,0 +1,62 @@
+import java.util.*;
+class Solution {
+    int maxScore = 0;
+    int[] info;
+    int[] answer = new int[11];
+    
+    public int[] solution(int n, int[] info) {
+        this.info = info;
+        dfs(0, new int[11], n);
+        
+        
+        if(maxScore == 0){
+            return new int[]{-1};
+        }
+        
+        return answer;
+    }
+    
+    
+    public void dfs(int index, int[] lion, int arrow){
+        if(index == 11){
+            lion[10] = arrow;
+            
+            int score = calculateScore(lion);
+            
+            if(score > maxScore){
+                maxScore = score;
+                answer = Arrays.copyOf(lion, lion.length);
+            } else if(score == maxScore){
+                for(int i = 10; i >= 0; i--){
+                    if(answer[i] == lion[i]){
+                        continue;
+                    }
+                    if(answer[i] < lion[i]){
+                        answer = Arrays.copyOf(lion,lion.length);
+                    }
+                    break;
+                }
+            }
+            return ;
+        }
+        
+        if(info[index] < arrow){
+            lion[index] = info[index] + 1;
+            dfs(index + 1, lion, arrow - lion[index]);
+            lion[index] = 0;
+        }
+        dfs(index + 1, lion, arrow);
+    }
+    
+    public int calculateScore(int[] lion){
+        int diff = 0;
+        for(int i = 0; i <= 9; i++){
+            if(lion[i] == 0 && info[i] == 0){
+                continue;
+            }
+            diff = lion[i] > info[i] ? diff + (10 - i) : diff - (10 - i);
+        }
+        
+        return diff;
+    }
+}
