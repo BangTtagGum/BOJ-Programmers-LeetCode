1f4106e6b48616a3c8849db95652b7a8ad2ab8dd
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-08-04 16:44:22 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 352.89 ms, Memory: 300 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[][] edges) {
+        Map<Integer, int[]> nodeCount = new HashMap<>();
+        
+        int[] answer = {0,0,0,0};
+        
+        Arrays.stream(edges).forEach(edge -> {
+            int a = edge[0];
+            int b = edge[1];
+            if(!nodeCount.containsKey(a))
+                nodeCount.put(a, new int[]{0,0});
+            if(!nodeCount.containsKey(b))
+                nodeCount.put(b, new int[]{0,0});
+            nodeCount.get(a)[0] += 1; 
+            nodeCount.get(b)[1] += 1; 
+        });
+        
+        int[] count;
+        for(int key : nodeCount.keySet()) {
+            count = nodeCount.get(key);
+            if(count[0]>=2 && count[1]==0) 
+                answer[0] = key;
+            else if(count[0]==0 && count[1]>0) 
+                answer[2]++;
+            else if(count[0]>=2 && count[1]>=2) 
+                answer[3]++;
+        }
+        answer[1] = nodeCount.get(answer[0])[0] - answer[2] - answer[3];
+        return answer;
+    }
+}
