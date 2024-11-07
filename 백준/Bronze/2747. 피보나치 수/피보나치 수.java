932c72b3960a2f1aa139f2f8e0b982c52d60a663
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-11-07 13:25:48 +0900

    [Bronze II] Title: 피보나치 수, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,15 +1,21 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[46];  //DP 배열
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        int x = 0, y = 1;
+        for (int i = 0; i < n; i++) {
+            int z = x + y;
+            x = y;
+            y = z;
         }
-        System.out.print(dp[n]);
+        System.out.println(x);
+
+        br.close();
     }
 }
