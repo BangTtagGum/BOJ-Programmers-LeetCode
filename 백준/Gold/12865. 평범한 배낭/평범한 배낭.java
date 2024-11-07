f29cffc6be20e19d434ca0077303eeea8a669d9b
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-11-07 13:46:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 192 ms, Memory: 54072 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,66 +1,43 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
-/**
- *  2023.08.03
- *  12865 평범한 배낭
- */
 public class Main {
+
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-
-        ArrayList<Thing> things = new ArrayList<>();
-        things.add(new Thing(0, 0));
+        int[][] items = new int[n + 1][2];
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            things.add(new Thing(w, v));
+            items[i][0] = Integer.parseInt(st.nextToken());
+            items[i][1] = Integer.parseInt(st.nextToken());
         }
-        int dy[][] = new int[n+1][k + 1];
 
-        for (int i = 1; i <= n; i++) {   // i는 물건의 인덱스
-            for (int j = 1; j <= k; j++) { // j는 가방의 허용 공간
-                dy[i][j] = dy[i - 1][j];
-                if (things.get(i).w <= j) {
-                    // 중복을 허용하지 않기 위해 dy[i-1]에 i번째 물건의 가치를 더함
-                    dy[i][j] = Math.max(dy[i-1][j - things.get(i).w] + things.get(i).v,dy[i-1][j]);    
+        int[][] storage = new int[n + 1][k + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int capacity = 0; capacity <= k; capacity++) {
+                if (i == 0 || capacity == 0) {
+                    storage[i][capacity] = 0;
+                } else if (items[i][0] <= capacity) {
+                    storage[i][capacity] = Math.max(
+                            storage[i - 1][capacity - items[i][0]] + items[i][1],
+                            storage[i - 1][capacity]);
+                } else {
+                    storage[i][capacity] = storage[i - 1][capacity];
                 }
-                
             }
         }
 
-        System.out.println(dy[n][k]);
+        System.out.println(storage[n][k]);
+        br.close();
     }
 }
-
-class Thing implements Comparable<Thing>{
-    int w;
-    int v;
-
-    public Thing(int w, int v) {
-        this.w = w;
-        this.v = v;
-    }
-
-
-    @Override
-    public int compareTo(Thing o) {
-        return this.w - o.w;
-    }
-}
-
-
-
-
-
-
