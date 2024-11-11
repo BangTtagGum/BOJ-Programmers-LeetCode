2c6f29874b47a00573d75c0dfa3a8e22528330fc
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-11-11 14:50:46 +0900

    [Gold II] Title: Dessert Café, Time: 620 ms, Memory: 94532 KB -BaekjoonHub

---
+++ 백준/Gold/20171. Dessert Café/Dessert Café.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    static StringTokenizer st;
+    static boolean[] visited;
+    static int count = 0;
+    static Map<Integer, List<Integer>> graph;
+    static Set<Integer> apartComplex;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        graph = new HashMap<>();
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            graph.putIfAbsent(a, new ArrayList<>());
+            graph.putIfAbsent(b, new ArrayList<>());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        apartComplex = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+
+        int ac1 = -1;
+        for (int i = 0; i < k; i++) {
+            Integer complexNum = Integer.valueOf(st.nextToken());
+            apartComplex.add(complexNum);
+
+            if (ac1 == -1) {
+                ac1 = complexNum;
+            }
+        }
+
+
+
+        visited = new boolean[n + 1];
+//        for (int i = 1; i <= n; i++) {
+//            // 아파트 단지에서 출발
+//            if (apartComplex.contains(i)) {
+//                dfs(i);
+//            }
+//        }
+        dfs(ac1);
+
+
+        System.out.println(count);
+    }
+
+    private static boolean dfs(int i) {
+        visited[i] = true;
+        boolean flag = false;
+        if (apartComplex.contains(i)) {
+            flag = true;
+        }
+        for (int next : graph.get(i)) {
+            if (visited[next]) {
+                continue;
+            }
+            if (apartComplex.contains(next)) {
+                flag = true;
+            }
+
+            if (dfs(next)) {
+                flag = true;
+            }
+
+        }
+        if (flag) {
+            count++;
+        }
+        return flag;
+    }
+
+}
